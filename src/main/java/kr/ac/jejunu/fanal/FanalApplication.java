fffc3ff5f674bfcb501ca25a778b80f848aedd76
WonBeomChoi <cwb2310@gmail.com>
2019-06-07 17:51:52 +0900

    remake repo

---
+++ src/main/java/kr/ac/jejunu/fanal/FanalApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.fanal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanalApplication.class, args);
+    }
+
+}
